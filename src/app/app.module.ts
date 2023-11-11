6ef58864446becaad8e8eddd3d8f217b7a14f5e7
julio diaz <juliocm17@gmail.com>
2023-11-11 15:04:34 -0600

    Create base petshop app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookAppointmentComponent } from './components/book-appointment/book-appointment.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ToysAndAccessoriesComponent } from './components/toys-and-accessories/toys-and-accessories.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookAppointmentComponent,
+    ContactUsComponent,
+    ToysAndAccessoriesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
